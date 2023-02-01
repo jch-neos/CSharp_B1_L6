e3713a635f7e3e8f0a5c2f4d58a9338b728d46c0
Julien Cheritat <julien.cheritat@neos-sdi.com>
2023-02-01 10:02:59 +0100

    inital commit

---
+++ exception/Benchmarks.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+using BenchmarkDotNet;
+using BenchmarkDotNet.Attributes;
+
+namespace exception {
+  public class Benchmarks {
+    string test = "12321a";
+    [Benchmark]
+    public int Try() {
+      if (int.TryParse(test, out var res)) return res;
+      else return int.MinValue;
+    }
+    [Benchmark]
+    public int Parse() {
+      try { return int.Parse(test); } 
+      catch { return int.MinValue; }
+    }
+    [Benchmark]
+    public int ParseMessage() {
+      try { return int.Parse(test); } 
+      catch(Exception e) { if(e.Message != null) {return int.MinValue;} return 0; }
+    }
+    [Benchmark]
+    public int ParseStack() {
+      try { return int.Parse(test); } 
+      catch(Exception e) { if(e.StackTrace != null) {return int.MinValue;} return 0; }
+    }
+    [Benchmark]
+    public int Parse5LevelRethrow(){
+      try { return ParseRethrow(5); } 
+      catch { return int.MinValue; }
+    }
+    public int ParseRethrow(int n){
+      if(n==0) return int.Parse(test);
+      try { return ParseRethrow(n-1); } 
+      catch { throw; }
+    }
+    [Benchmark]
+    public int Parse5LevelNothrow(){
+      try { return ParseNoThrow(5); } 
+      catch { return int.MinValue; }
+    }
+    public int ParseNoThrow(int n){
+      if(n==0) return int.Parse(test);
+      return ParseNoThrow(n-1); 
+    }
+
+    // [Benchmark]
+    // public string Scenario1()
+    // {
+    //     try {
+    //       throw new Exception();
+    //     } catch(Exception e) {
+    //       return e.StackTrace;
+    //     }
+    // }
+
+    // [Benchmark]
+    // public string Scenario2()
+    // {
+    //     return new StackTrace().GetFrame(1).ToString();
+    // }
+  }
+}
---
+++ throw/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Net;
+using System.Web;
+
+class Pr{
+    static void Main(){
+        new Cl().A();
+    }
+}
+
+class Cl {
+    
+
+    public void A(){
+    
+        try {
+            B();
+        } catch (Exception e){
+            Console.WriteLine(e.Source);
+            Console.WriteLine(e.TargetSite);
+            Console.WriteLine(e.StackTrace); 
+        }
+        
+        Console.WriteLine(new Exception().TargetSite);
+
+    }
+
+        void B(){
+    
+        try {
+            C();
+        } catch (Exception e){
+            Console.WriteLine(e.Source);
+            Console.WriteLine(e.TargetSite);
+            Console.WriteLine(e.StackTrace);
+            throw e;
+        }
+    
+    }
+    void C(){
+    
+        try {
+            D();
+        } catch (Exception e){
+            Console.WriteLine(e.Source);
+            Console.WriteLine(e.TargetSite);
+            Console.WriteLine(e.StackTrace);
+            throw;
+        }
+    
+    }
+    void D() => throw new Exception();
+
+}
